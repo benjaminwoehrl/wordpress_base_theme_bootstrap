8ceddcd5bf2a88c171e11a52128e396b685bd64d
ruction <benjaminwoehrl@googlemail.com>
2014-10-11 16:57:03 +0200

    Added WordPress Theme.

---
+++ footer.php
@@ -0,0 +1,6 @@
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<title><?php wp_title(); ?></title>
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+
+		<!-- Unterstützung für Media Queries und HTML5-Elemente im Internet Explorer über HTML5 shim und Respond.js -->
+		<!-- ACHTUNG: Respond.js funktioniert nicht, wenn du die Seite über file:// aufrufst -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<?php wp_head(); ?>
+	</head>
+	<body>
